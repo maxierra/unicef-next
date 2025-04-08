0e5ba5f3a24978c848f2d82cf4556f6575295744
maxierra <145459810+maxierra@users.noreply.github.com>
2025-04-08 14:54:15 -0300

    Add files via upload
    3

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   // Configuración para despliegue en Render
   output: 'standalone',
-  // Indicar explícitamente la ubicación del directorio src
   poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true
